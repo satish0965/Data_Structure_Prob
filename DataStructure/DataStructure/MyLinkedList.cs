62a276c062fdb2c13494cf996559a374062b712b
Satish Patil <satish.patil7017@gmail.com>
2022-11-12 08:15:34 +0530

    Added Creating Linked List

---
+++ DataStructure/DataStructure/MyLinkedList.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStrucure
+{
+    class MyLinkedList
+    {
+        public Node head;
+        public void AddLast(int data)
+        {
+            Node newNode = new Node(data);
+            if (this.head == null)
+            {
+                this.head = newNode;
+            }
+            else
+            {
+                Node temp = this.head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = newNode;
+            }
+            Console.WriteLine("{0} is inserted into LinkedList", newNode.data);
+        }
+        public void Display()
+        {
+            Console.WriteLine("Displaying Nodes:");
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("LinkedList is Empty");
+                return;
+            }
+            else
+            {
+                while (temp != null)
+                {
+                    Console.Write(" " + temp.data + " ");
+                    temp = temp.next;
+                }
+            }
+        }
+    }
+}
---
+++ DataStructure/DataStructure/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStrucure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the LinkedList Program");
+
+            MyLinkedList linkedList = new MyLinkedList();
+            linkedList.AddLast(56);
+            linkedList.AddLast(30);
+            linkedList.AddLast(70);
+            linkedList.Display();
+
+            Console.ReadLine();
+        }
+    }
+}
+//UC-1
+//Lets create a simple Linked List of 56, 30 and 70
+
+//Result
+//Welcome to the LinkedList Program
+//56 is inserted into LinkedList
+//30 is inserted into LinkedList
+//70 is inserted into LinkedList
+//Displaying Nodes:
+// 56  30  70
